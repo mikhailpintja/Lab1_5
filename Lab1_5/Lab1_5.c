256e90e64c63bee334c2773a54054f565879233d
Mikhail.pintja <Mikhail.pintja2@gmail.com>
2023-04-14 15:07:41 +0300

    Добавьте файлы проекта.

---
+++ Lab1_5/Lab1_5.c
@@ -0,0 +1,22 @@
+﻿// Lab1_5.c : Визначення цілого числа хвилин по заданим секундам//
+
+#include<locale.h>
+#include <stdio.h>
+#include <conio.h>
+
+int m;
+
+int main(void)
+{
+	unsigned int N;
+	setlocale(LC_CTYPE, "ukr");
+	printf("\n Введiть кiлькiсть секунд  N=");
+	scanf_s("%u", &N);
+
+	m = N / 60;
+
+	printf("\n Введена к-ть секунд мiстить повних: %d хв.", m);
+
+	int getch(); getch();
+	return 0;
+}
